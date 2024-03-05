37a98b0373cb3ab7fa4f93d4cfec2d9f3aa36e7e
Abhijeet Chatterjee <97887115+Abhijxxt@users.noreply.github.com>
2024-03-06 00:53:23 +0530

    Not working
    Trying to add a common header file with link tags which won't work
    Maybe having static logo and title page may work but not any Links.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
+import { Link } from 'react-router-dom';
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import About from './pages/About';
-import Info from './pages/Info';
-import NotFoundPage from './pages/404';
-
-const routers = createBrowserRouter([
-  {
-    path : "/",
-    element : <Home />,
-  },
-  {
-    path : '/contact',
-    element : <Contact />
-  },
-  {
-    path : '/about',
-    element : <About />
-  },
-  {
-    path : '/info',
-    element : <Info />
-  },
-  {
-    path : '*',
-    element : <NotFoundPage />,
-  }
-]
-);
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-xl text-center p-3 bg-slate-500 text-yellow-200">This is the common header for all the pages.</h1>
-      <RouterProvider router={routers} />
+      <header>
+        <h1 className="text-xl text-center p-3 bg-slate-500 text-yellow-200">This is the common header for all the pages.</h1>
+        <p>This is a header file that will be attached to every route</p>
+      </header>
+      <nav className="bg-slate-800 p-8">
+                <Link to={'/'} className=" m-2 p-2 bg-fuchsia-300">Home Button</Link>
+                <Link to={'/about'} className=" m-2 p-2 bg-fuchsia-300">About Button</Link>
+                <Link to={'/contact'} className=" m-2 p-2 bg-fuchsia-300">Contact Button</Link>
+                <Link to={'/info'} className=" m-2 p-2 bg-fuchsia-300">Info Button</Link>
+            </nav>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Info from './pages/Info';
+import NotFoundPage from './pages/404';
+
+const routers = createBrowserRouter([
+  {
+    path : "/",
+    element : <Home />,
+  },
+  {
+    path : '/contact',
+    element : <Contact />
+  },
+  {
+    path : '/about',
+    element : <About />
+  },
+  {
+    path : '/info',
+    element : <Info />
+  },
+  {
+    path : '*',
+    element : <NotFoundPage />,
+  }
+]
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <RouterProvider router={routers} />
   </React.StrictMode>
 );
 
