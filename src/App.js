1ba1e019ea34b11386425a95387b7836637e687d
Abhijeet Chatterjee <97887115+Abhijxxt@users.noreply.github.com>
2024-03-06 00:09:28 +0530

    Installed react-router-dom and created basic pages
    Created basic About, Contact, Home, 404 pages and created some Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Info from './pages/Info';
+import NotFoundPage from './pages/404';
+
+const routers = createBrowserRouter([
+  {
+    path : "/",
+    element : <Home />,
+  },
+  {
+    path : '/contact',
+    element : <Contact />
+  },
+  {
+    path : '/about',
+    element : <About />
+  },
+  {
+    path : '/info',
+    element : <Info />
+  },
+  {
+    path : '*',
+    element : <NotFoundPage />,
+  }
+]
+);
 
 function App() {
   return (
     <div className="App">
-  
+      <RouterProvider router={routers} />
     </div>
   );
 }
---
+++ src/pages/Info.js
@@ -0,0 +1,7 @@
+export default function Info() {
+    return (
+        <div>
+            <h1>Information page</h1>
+        </div>
+    )
+}
